c9f810eb7cf22e27fcbfaf7fb585bc6754ac9857
youin10 <45347334+youin10@users.noreply.github.com>
2019-10-14 13:00:16 +0900

    Update __init__.py

--- Eight_Puzzle_Problem/search/__init__.py
+++ Eight_Puzzle_Problem/search/__init__.py
@@ -4,3 +4,4 @@ from .search import depth_first_tree_search
 from .search import breadth_first_tree_search
 from .search import depth_limited_search
 from .search import iterative_deepening_search
+from .search import astar_search
