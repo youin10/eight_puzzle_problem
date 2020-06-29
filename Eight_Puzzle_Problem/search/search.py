05ea08a1c77c886938083e2653869a871f18ff56
youin10 <45347334+youin10@users.noreply.github.com>
2020-06-30 06:03:59 +0900

    Update search.py

--- Eight_Puzzle_Problem/search/search.py
+++ Eight_Puzzle_Problem/search/search.py
@@ -181,3 +181,111 @@ def memoize(fn, slot=None, maxsize=32):
             return fn(*args)
 
     return memoized_fn
+
+
+class CSP(search.Problem):
+    def __init__(self, variables, domains, neighbors, constraints):
+        """Construct a CSP problem. If variables is empty, it becomes domains.keys()."""
+        super().__init__(())
+        variables = variables or list(domains.keys())
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.curr_domains = None
+        self.nassigns = 0
+
+    def assign(self, var, val, assignment):
+        """Add {var: val} to assignment; Discard the old value if any."""
+        assignment[var] = val
+        self.nassigns += 1
+
+    def unassign(self, var, assignment):
+        """Remove {var: val} from assignment.
+        DO NOT call this if you are changing a variable to a new value;
+        just call assign for that."""
+        if var in assignment:
+            del assignment[var]
+
+    def nconflicts(self, var, val, assignment):
+        """Return the number of conflicts var=val has with other variables."""
+
+        # Subclasses may implement this more efficiently
+        def conflict(var2):
+            return var2 in assignment and not self.constraints(var, val, var2, assignment[var2])
+
+        return count(conflict(v) for v in self.neighbors[var])
+
+    def display(self, assignment):
+        """Show a human-readable representation of the CSP."""
+        # Subclasses can print in a prettier way, or display with a GUI
+        print(assignment)
+
+    # These methods are for the tree and graph-search interface:
+
+    def actions(self, state):
+        """Return a list of applicable actions: non conflicting
+        assignments to an unassigned variable."""
+        if len(state) == len(self.variables):
+            return []
+        else:
+            assignment = dict(state)
+            var = first([v for v in self.variables if v not in assignment])
+            return [(var, val) for val in self.domains[var]
+                    if self.nconflicts(var, val, assignment) == 0]
+
+    def result(self, state, action):
+        """Perform an action and return the new state."""
+        (var, val) = action
+        return state + ((var, val),)
+
+    def goal_test(self, state):
+        """The goal is to assign all variables, with all constraints satisfied."""
+        assignment = dict(state)
+        return (len(assignment) == len(self.variables)
+                and all(self.nconflicts(variables, assignment[variables], assignment) == 0
+                        for variables in self.variables))
+
+    # These are for constraint propagation
+
+    def support_pruning(self):
+        """Make sure we can prune values from domains. (We want to pay
+        for this only if we use it.)"""
+        if self.curr_domains is None:
+            self.curr_domains = {v: list(self.domains[v]) for v in self.variables}
+
+    def suppose(self, var, value):
+        """Start accumulating inferences from assuming var=value."""
+        self.support_pruning()
+        removals = [(var, a) for a in self.curr_domains[var] if a != value]
+        self.curr_domains[var] = [value]
+        return removals
+
+    def prune(self, var, value, removals):
+        """Rule out var=value."""
+        self.curr_domains[var].remove(value)
+        if removals is not None:
+            removals.append((var, value))
+
+    def choices(self, var):
+        """Return all values for var that aren't currently ruled out."""
+        return (self.curr_domains or self.domains)[var]
+
+    def infer_assignment(self):
+        """Return the partial assignment implied by the current inferences."""
+        self.support_pruning()
+        return {v: self.curr_domains[v][0]
+                for v in self.variables if 1 == len(self.curr_domains[v])}
+
+    def restore(self, removals):
+        """Undo a supposition and all inferences from it."""
+        for B, b in removals:
+            self.curr_domains[B].append(b)
+
+    # This is for min_conflicts search
+
+    def conflicted_vars(self, current):
+        """Return a list of variables in current assignment that are in conflict"""
+        return [var for var in self.variables
+                if self.nconflicts(var, current[var], current) > 0]
+
